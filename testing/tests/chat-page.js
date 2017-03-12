8f3a616f75e878e59f77aaa30bb4c1ab2420df0a
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2017-03-12 12:06:50 -0500

    Write initial tests for chat page

--- testing/tests/chat-page.js
+++ testing/tests/chat-page.js
@@ -7,6 +7,25 @@ module.exports = {
       .pause(1000);
 
     client.expect.element('body').to.be.present;
+    client.expect.element('.online-users').to.be.present;
+    client.expect.element('.chat-area').to.be.present;
+    client.expect.element('.chat-list').to.be.present;
+    client.expect.element('.chat-form').to.be.present;
+    client.assert.value('.users-list:first-child .user-text', "guest_0");
+
+    client
+      .setValue('.chat-text', 'Lorem ipsum')
+      .click('.chat-submit')
+      .pause(1000)
+      .assert.elementPresent('.chat-list li')
+      .assert.value('.chat-list:first-child textarea', 'Lorem ipsum');
+
+      client
+        .setValue('.chat-text', 'Ut lobortis')
+        .submitForm('form.chat-input')
+        .pause(10000)
+        .assert.elementPresent('.chat-list:nth-child(2)')
+        .assert.value('.chat-list:nth-child(2) textarea', 'Ut lobortis');
 
     client.end();
   }
